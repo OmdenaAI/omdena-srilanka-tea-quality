2cf01b18b6687e671aa78bd03da1c5a7ebb39aec
Iambusayor <hawobade@gmail.com>
2021-09-05 01:33:57 +0100

    busayor

---
+++ task_4_model_building/streamlit-webapp/app.py
@@ -0,0 +1,93 @@
+# import libraries
+import streamlit as st
+from model import fresh_model, withered_model
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
+from detector import annotate_leaves, predict_leaves, get_leaves
+from os import path
+
+
+
+# page icon
+st.set_page_config(page_title='Tea-Leaves Classification', page_icon='🌿')
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+# Instatiates neccessary model
+@st.cache(allow_output_mutation=True)
+def load_fresh_model():
+    model_fresh = fresh_model()
+    return model_fresh
+
+@st.cache(allow_output_mutation=True)
+def load_withered_model():
+    model_withered = withered_model()
+    return model_withered
+
+fresh_model = load_fresh_model()
+withered_model = load_withered_model()
+
+
+
+
+
+def main():
+
+    """Tea-Leaf Classification"""
+
+    st.title('Tea-Leaf Classification')
+    st.write('*Classify tea leaves based on region and category*')
+
+    # select leaf category
+    categories = ['Fresh', 'Withered']
+    choice = st.sidebar.selectbox('Select Category', categories)
+
+    if choice == 'Fresh':
+        
+        # input image
+        file = st.file_uploader("Please upload tea-leaf image", type=['jpg', 'png', 'jpeg'])
+
+        if file is None:
+            st.text('Please upload an image file')
+        else:
+            # read input image
+            image = Image.open(file)
+            # get detections
+            response, num_leaves = get_leaves(image=file)
+            st.info(f'Number of Tea-Leaves found:   {num_leaves}')
+            if num_leaves > 0:
+                # annotate leaves
+                annotated_img = annotate_leaves(image=image, response=response)
+                st.image(annotated_img, use_column_width=True)
+                if st.button('Predict'):
+                    # get classifications
+                    annotated_img = predict_leaves(image=image, response=response, model=fresh_model)
+                    st.image(annotated_img, use_column_width=True)
+                
+
+    elif choice == 'Withered':
+
+        # input image
+        file = st.file_uploader("Please upload tea-leaf image", type=['jpg', 'png', 'jpeg'])
+
+        if file is None:
+            st.text('Please upload an image file')
+        else:
+            # read input image
+            image = Image.open(file)
+            # get detections
+            response, num_leaves = get_leaves(image=file)
+            st.info(f'Number of Tea-Leaves found:   {num_leaves}')
+            if num_leaves > 0:
+                # annotate leaves
+                annotated_img = annotate_leaves(image=image, response=response)
+                st.image(annotated_img, use_column_width=True)
+                if st.button('Predict'):
+                    # get classifications
+                    annotated_img = predict_leaves(image=image, response=response, model=withered_model)
+                    st.image(annotated_img, use_column_width=True)
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ task_4_model_building/streamlit-webapp/detector.py
@@ -0,0 +1,80 @@
+# Import libraries
+import numpy as np
+import requests
+from PIL import Image, ImageDraw, ImageFont
+from tqdm import tqdm
+
+
+classes = ['Below-Best', 'Best', 'Poor']
+
+# pass image into classifier model
+def predict(image, model):
+    predictions = model.predict(image)
+    class_idx = np.argmax(predictions)
+    print(predictions)
+    return classes[class_idx]
+
+# use deepstack wrapper to connect detection model for detection
+def get_leaves(image):
+    image_data = image.getvalue()
+    response = requests.post("http://localhost:80//v1/vision/custom/tea_leaves",files={"image":image_data}).json()
+    return response, len(response['predictions']) # returns detections and number of detections 
+
+
+def annotate_leaves(image, response):   
+	# convert image to RGB format
+    image = image.convert('RGB')
+	# create imagedraw object
+    draw = ImageDraw.Draw(image)
+	# create font
+    roboto_font = ImageFont.truetype('/home/busayor/Shiii/Omdena/Sri-Lanka/web-app/Oswald,Roboto/Roboto/Roboto-Bold.ttf', 40)
+
+	# annotate every detection found and labels them
+    for  detection in tqdm(response["predictions"], total=len(response['predictions'])):
+            x_min = detection['x_min']
+            y_min = detection['y_min']
+            x_max = detection['x_max']
+            y_max = detection['y_max']
+
+            label = detection['label']
+
+            draw.rectangle((x_min, y_min, x_max, y_max), outline=(230, 57, 70), width=7)
+            draw.text((x_min, y_min-50), text=label, font=roboto_font, fill=(242, 92, 84))
+
+    
+    del draw
+    return image 
+
+
+def predict_leaves(image, response, model, size=(224, 224)):
+	# convert image to RGB format    
+    image = image.convert('RGB')
+	# create imagedraw object
+    draw = ImageDraw.Draw(image)
+	# create font
+    roboto_font = ImageFont.truetype('/home/busayor/Shiii/Omdena/Sri-Lanka/web-app/Oswald,Roboto/Roboto/Roboto-Bold.ttf', 40)
+    
+	# crop detected leaves and pass into classifier for classification, returns annotation with class label
+    for  detection in tqdm(response["predictions"], total=len(response['predictions'])):
+            x_min = detection['x_min']
+            y_min = detection['y_min']
+            x_max = detection['x_max']
+            y_max = detection['y_max']
+
+            label = detection['label']
+
+            leaf = image.crop((x_min,y_min,x_max,y_max))
+            leaf = leaf.resize(size, Image.ANTIALIAS)
+            leaf = np.asarray(leaf)
+            leaf = leaf[np.newaxis, ...]
+
+            pred = predict(leaf, model)
+
+
+            draw.rectangle((x_min, y_min, x_max, y_max), outline=(230, 57, 70), width=7)
+            draw.text((x_min, y_min-50), text=pred, fill=(0, 41, 107), font=roboto_font)
+            
+            
+    
+    del draw
+    return image
