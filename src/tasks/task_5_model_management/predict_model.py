0f7c49d97ffef589328738592435e87f1015c0c5
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2021-09-24 23:43:32 +0530

    Create predict_model.py

---
+++ src/tasks/task_5_model_management/predict_model.py
@@ -0,0 +1,28 @@
+import boto3
+from load_dataset import load_dataset
+
+app_name = 'mlflow-omd'
+region = 'us-east-1'
+
+if __name__ == '__main__':
+    sm = boto3.client('sagemaker', region_name=region)
+    smrt = boto3.client('runtime.sagemaker', region_name=region)
+
+    # Check endpoint status
+    endpoint = sm.describe_endpoint(EndpointName=app_name)
+    print("Endpoint status: ", endpoint["EndpointStatus"])
+    
+    # Load test set
+    x_train, x_test, y_train, y_test = load_dataset(
+        'our-dataset', ';'
+    )
+    
+    # Predict first 10 samples
+    input_data = x_test[:10].to_json(orient="split")
+    prediction = smrt.invoke_endpoint(
+        EndpointName=app_name,
+        Body=input_data,
+        ContentType='application/json; format=pandas-split'
+    )
+    prediction = prediction['Body'].read().decode("ascii")
+    print(prediction)
