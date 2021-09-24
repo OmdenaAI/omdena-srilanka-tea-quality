b9ddf69fe1ef658594d8b406413d03a120697b7c
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2021-09-24 23:12:23 +0530

    Create train_data.py

---
+++ src/tasks/task_5_model_management/train_data.py
@@ -0,0 +1,18 @@
+import mlflow.xgboost
+import our_model as omd
+from load_dataset import load_dataset
+
+if __name__ == '__main__':
+    mlflow.set_experiment('direct-marketing-xgboost')
+    with mlflow.start_run(run_name='direct-marketing-xgboost-basic') as run:
+        x_train, x_test, y_train, y_test = load_dataset(
+            'our-dataset', ';'
+        )
+        cls = omd.our_model(some parameters)
+        cls.fit(x_train, y_train)
+        auc = cls.score(x_test, y_test)
+        print("AUC ", auc)
+        mlflow.log_metric("auc", auc)
+
+        mlflow.our_model.log_model(cls, "name")
+        mlflow.end_run()
