5156f226bf246b37a267f2f4957f56aee1ce7f23
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2021-09-25 11:36:06 +0530

    added num

--- src/tasks/task_6_model_serving/FLASK_API_Project/multi_route_flask.py
+++ src/tasks/task_6_model_serving/FLASK_API_Project/multi_route_flask.py
@@ -13,11 +13,11 @@ def index():
 		some_json = request.get_json()
 		return jsonify({'you sent' : some_json}) , 201
 	else:
-		return jsonify({'about'} : "Hello World!")	
+		return jsonify({'about' : "Hello World!"})	
 
 # If you will not specifically declare any method then the default will be GET
 @app.route('/multl/<int:num>' , methods = ['GET'])	
-def get_multiply():
+def get_multiply(num=None):
 	return jsonify({"result" : num * 10})
 
 if __name__ == '__main__':
