ee46326de255bdb253b5e3a91a3647b37592f593
Peeush Agarwal <agarwal.peeush.developer@gmail.com>
2021-10-04 10:34:17 +0530

    Rename server.py to application.py to enable AWS EB to pick automatically

--- task_6_Model_Serving/server/src/application.py
+++ task_6_Model_Serving/server/src/application.py
@@ -5,4 +5,7 @@ from api.app import create_app
 
 env = os.getenv("FLASK_ENV")
 # print(env)
-app = create_app(env)
+application = create_app(env)
+
+if __name__ == "__main__":
+    application.run()
