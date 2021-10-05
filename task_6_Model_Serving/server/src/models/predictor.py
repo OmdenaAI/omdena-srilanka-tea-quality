a2fe979aa00d00351ba205f16ac0a8bd8547a206
Peeush Agarwal <agarwal.peeush.developer@gmail.com>
2021-10-05 09:20:29 +0530

    Add actual model file to preprocess and make inference from model

--- task_6_Model_Serving/server/src/models/predictor.py
+++ task_6_Model_Serving/server/src/models/predictor.py
@@ -1,15 +1,31 @@
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+from flask import current_app
+
+
 class Predictor:
     def __init__(self):
-        pass
+        model_filepath = current_app.config['MODEL_FILE']
+        print(model_filepath)
+        try:
+            self.model = load_model(model_filepath)
+        except:
+            raise Exception('Could not load the model')
 
     def predict(self, image_file):
-        # TODO: Convert image_file to numpy array
-        img_arr = None
+        img = image.load_img(image_file, target_size=(224, 224))
+        img_arr = image.img_to_array(img)
         preprocessed = self.__preprocess__(img_arr)
         return self.__inference__(preprocessed)
 
     def __preprocess__(self, img_arr):
-        pass
+        # Scaling
+        x = img_arr/255
+        x = np.expand_dims(x, axis=0)
+        return x
 
     def __inference__(self, img_arr_preprocessed):
-        pass
+        print(img_arr_preprocessed.shape)
+        predict = self.model.predict(img_arr_preprocessed)
+        print(predict)
