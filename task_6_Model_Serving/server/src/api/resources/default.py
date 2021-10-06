59ac5feb22ee621e1224e9ce3d77a05c3b153e8d
Peeush Agarwal <agarwal.peeush.developer@gmail.com>
2021-10-06 09:22:24 +0530

    Move default route swagger doc to individual file

--- task_6_Model_Serving/server/src/api/resources/default.py
+++ task_6_Model_Serving/server/src/api/resources/default.py
@@ -1,36 +1,17 @@
 from flask_restful import Resource
+from flasgger import swag_from
 from http import HTTPStatus
 
 from api.app import api
 
 
 class DefaultResource(Resource):
-    """
-    Handle default route
-    """
-
-    def get(self):
-        """
-        Default api route handler
-        ---
-        tags:
-        - Home
-        responses:
-          200:
-            description: Simple welcome message
-            schema:
-              properties:
-                status:
-                  type: string
-                  description: Status of the request
-                msg:
-                  type: string
-                  description: Status msg
-        """
-        return {
-            'status': 'success',
-            'msg': 'Welcome to the Tea Leaf API!'
-        }, HTTPStatus.OK
+  """
+  Default route handler
+  """
+  @swag_from("../docs/default/get.yml")
+  def get(self):
+    return 'Welcome to the Tea Leaf API!', HTTPStatus.OK
 
 
 api.add_resource(DefaultResource, '/api/', endpoint='home')
