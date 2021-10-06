7d6d6feaaf4189dede1cabf874a2aec0766676c2
Peeush Agarwal <agarwal.peeush.developer@gmail.com>
2021-10-06 09:27:45 +0530

    Move Inference route swagger doc to individual file

--- task_6_Model_Serving/server/src/api/resources/inference.py
+++ task_6_Model_Serving/server/src/api/resources/inference.py
@@ -4,10 +4,11 @@ It accepts an image file and returns the result of prediction from CNN model.
 '''
 from flask import request, Response, current_app
 from flask_restful import Resource, reqparse
+from flasgger import swag_from
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import FileStorage
-import os
 from http import HTTPStatus
+import os
 import imghdr
 
 from api.app import api
@@ -24,96 +25,62 @@ parser.add_argument('file',
 
 
 class InferencesResource(Resource):
-    """
-    InferencesApi Resource to handle POST request on '/inferences' endpoint.
-    """
+  """
+  InferencesApi Resource to handle POST request on '/inferences' endpoint.
+  """
 
-    def __init__(self, **kwargs):
-        self.allowed_exts = current_app.config['ALLOWED_EXTENSIONS']
-        self.logger = kwargs.get('logger')
+  def __init__(self, **kwargs):
+    self.allowed_exts = current_app.config['ALLOWED_EXTENSIONS']
+    self.logger = kwargs.get('logger')
 
-        # print(self.allowed_exts)
-        self.storage_handler = Storage()
-        self.predictor = Predictor()
+    # print(self.allowed_exts)
+    self.storage_handler = Storage()
+    self.predictor = Predictor()
 
-    def post(self):
-        """
-        Upload image to get inference from model
-        ---
-        tags:
-        - Inference
-        consumes:
-        - multipart/form-data  
-        parameters:
-        - name: file
-          required: true
-          in: formData
-          type: file
-          description: Upload your file.
-        responses:
-          400:
-            description: Bad request
-            schema:
-              properties:
-                status:
-                  type: string
-                  description: Status of the request
-                msg:
-                  type: string
-                  description: Error msg of the prediction
-          200:
-            description: Status of the uploaded image
-            schema:
-              properties:
-                status:
-                  type: string
-                  description: Status of the request
-                msg:
-                  type: string
-                  description: Status msg of the prediction
-        """
+  @swag_from("../docs/inference/post.yml")
+  def post(self):
+    # Parse the request arguments
+    args = parser.parse_args()
+    # Validation on 'file' argument in the request
+    if args['file'] == "":
+      return self.__get_error_status__('No file found'), HTTPStatus.BAD_REQUEST
 
-        # Parse the request arguments
-        args = parser.parse_args()
-        # Validation on 'file' argument in the request
-        if args['file'] == "":
-            return {
-                'status': 'error',
-                'msg': 'No file found'
-            }, HTTPStatus.BAD_REQUEST
+    # read image file from the stream
+    uploaded_file = args['file']
+    filename = secure_filename(uploaded_file.filename).lower()
+    if filename != '':
+      # print(filename)
+      file_ext = os.path.splitext(filename)[1]
+      # print(file_ext)
+      if file_ext not in self.allowed_exts or \
+        file_ext != self.__validate_image__(uploaded_file.stream):
+        return self.__get_error_status__('Invalid file'), HTTPStatus.BAD_REQUEST
+    self.storage_handler.save_file(
+      uploaded_file, filename, request.mimetype)
 
-        # read image file from the stream
-        uploaded_file = args['file']
-        filename = secure_filename(uploaded_file.filename).lower()
-        if filename != '':
-            # print(filename)
-            file_ext = os.path.splitext(filename)[1]
-            # print(file_ext)
-            if file_ext not in self.allowed_exts or \
-                    file_ext != self.__validate_image__(uploaded_file.stream):
-                return {
-                    'status': 'error',
-                    'msg': 'Invalid file'
-                }, HTTPStatus.BAD_REQUEST
-        self.storage_handler.save_file(
-            uploaded_file, filename, request.mimetype)
+    result = self.predictor.predict(uploaded_file)
+    # TODO: Prepare response from the result above.
 
-        result = self.predictor.predict(uploaded_file)
-        # TODO: Prepare response from the result above.
+    # Return response json
+    return {
+      'status': 'success',
+      'msg': 'image uploaded',
+      'data': {}
+    }, HTTPStatus.OK
 
-        # Return response json
-        return {
-            'status': 'success',
-            'msg': 'image uploaded'
-        }, HTTPStatus.OK
-
-    def __validate_image__(self, stream):
-        header = stream.read(512)
-        stream.seek(0)
-        format = imghdr.what(None, header)
-        if not format:
-            return None
-        return '.' + (format if format != 'jpeg' else 'jpg')
+  def __get_error_status__(self, error_msg):
+    return {
+      'status': 'error',
+      'msg': error_msg
+    }
+  
+  def __validate_image__(self, stream):
+    header = stream.read(512)
+    stream.seek(0)
+    format = imghdr.what(None, header)
+    if not format:
+      return None
+    return '.' + (format if format != 'jpeg' else 'jpg')
 
 
 api.add_resource(InferencesResource, '/api/inferences')
