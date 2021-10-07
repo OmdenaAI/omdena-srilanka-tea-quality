6fd2221ff268feeb3cb7bf42a84dca35a1ff0c25
Charuka Samarakoon <charuka@protonmail.com>
2021-10-08 03:31:36 +0530

    Setup connectivity_plus

--- task_6_Model_Serving/client/flutter/lib/main.dart
+++ task_6_Model_Serving/client/flutter/lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:omdena_srilanka_tea_quality_client/providers/result_provider.dart';
 import 'package:provider/provider.dart';
@@ -8,8 +11,49 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // connectivity_plus
+  final Connectivity _connectivity = Connectivity();
+  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+  // setting initial state to not none to avoid "went online" notification at startup
+  ConnectivityResult _prevConnectivityStatus = ConnectivityResult.wifi;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // trigger when connection type changes
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen((ConnectivityResult result) {
+      if (result == ConnectivityResult.none) {
+        if (_prevConnectivityStatus != ConnectivityResult.none) {
+          // user has gone offline
+          print("User gone offline");
+        }
+      } else {
+        if (_prevConnectivityStatus == ConnectivityResult.none) {
+          // user has came online from offline state
+          print("User came online");
+        }
+      }
+
+      _prevConnectivityStatus = result;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    _connectivitySubscription.cancel();
+  }
 
   @override
   Widget build(BuildContext context) {
