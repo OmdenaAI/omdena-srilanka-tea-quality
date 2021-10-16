bf9091682ff8438680efa0bfee34db1c194f7ff2
Peeush Agarwal <agarwal.peeush.developer@gmail.com>
2021-10-16 13:40:21 +0530

    Add "main.py" and update "detect.py" for detection in the image

--- task_6_Model_Serving/azure-ml-pipeline/detect.py
+++ task_6_Model_Serving/azure-ml-pipeline/detect.py
@@ -23,7 +23,7 @@ from utils.plots import plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized
 
 
-def detect(save_img=False):
+def detect(opt, save_img=False):
     source, weights, view_img, save_txt, imgsz = (
         opt.source,
         opt.weights,
@@ -41,9 +41,9 @@ def detect(save_img=False):
     save_dir = Path(
         increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok)
     )  # increment run
-    (save_dir / "labels" if save_txt else save_dir).mkdir(
-        parents=True, exist_ok=True
-    )  # make dir
+    save_detected_dir = save_dir / "detected"
+    save_detected_dir.mkdir(parents=True, exist_ok=True)  # make dir
+    print(save_detected_dir)
 
     # Initialize
     set_logging()
@@ -83,6 +83,7 @@ def detect(save_img=False):
     img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
     _ = model(img.half() if half else img) if device.type != "cpu" else None  # run once
     for path, img, im0s, vid_cap in dataset:
+        img_ext = path.split(".")[-1]
         img = torch.from_numpy(img).to(device)
         img = img.half() if half else img.float()  # uint8 to fp16/32
         img /= 255.0  # 0 - 255 to 0.0 - 1.0
@@ -115,9 +116,6 @@ def detect(save_img=False):
                 p, s, im0 = Path(path), "", im0s
 
             save_path = str(save_dir / p.name)
-            txt_path = str(save_dir / "labels" / p.stem) + (
-                "_%g" % dataset.frame if dataset.mode == "video" else ""
-            )
             s += "%gx%g " % img.shape[2:]  # print string
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             if len(det):
@@ -130,18 +128,31 @@ def detect(save_img=False):
                     s += "%g %ss, " % (n, names[int(c)])  # add to string
 
                 # Write results
+                counter = 0
                 for *xyxy, conf, cls in reversed(det):
-                    if save_txt:  # Write to file
-                        xywh = (
-                            (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn)
-                            .view(-1)
-                            .tolist()
-                        )  # normalized xywh
-                        line = (
-                            (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)
-                        )  # label format
-                        with open(txt_path + ".txt", "a") as f:
-                            f.write(("%g " * len(line)).rstrip() % line + "\n")
+                    xywh = (
+                        (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn)
+                        .view(-1)
+                        .tolist()
+                    )  # normalized xywh
+                    line = (
+                        (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)
+                    )  # label format
+
+                    # Crop a xywh subimage in the top left corner
+                    c1, c2 = (int(xyxy[0]), int(xyxy[1])), (
+                        int(xyxy[2]),
+                        int(xyxy[3]),
+                    )
+                    raw_img = cv2.imread(path)
+                    # raw_img = cv2.cvtColor(raw_img, cv2.COLOR_BGR2RGB)
+                    cropped_image = raw_img[c1[1] : c2[1], c1[0] : c2[0]]
+                    # print(i)
+                    cv2.imwrite(
+                        str(save_detected_dir / f"det_{counter}.{img_ext}"),
+                        cropped_image,
+                    )
+                    counter = counter + 1
 
                     if save_img or view_img:  # Add bbox to image
                         label = "%s %.2f" % (names[int(cls)], conf)
@@ -191,59 +202,4 @@ def detect(save_img=False):
 
     print("Done. (%.3fs)" % (time.time() - t0))
 
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--weights", nargs="+", type=str, default="yolov5s.pt", help="model.pt path(s)"
-    )
-    parser.add_argument(
-        "--source", type=str, default="data/images", help="source"
-    )  # file/folder, 0 for webcam
-    parser.add_argument(
-        "--img-size", type=int, default=640, help="inference size (pixels)"
-    )
-    parser.add_argument(
-        "--conf-thres", type=float, default=0.25, help="object confidence threshold"
-    )
-    parser.add_argument(
-        "--iou-thres", type=float, default=0.45, help="IOU threshold for NMS"
-    )
-    parser.add_argument(
-        "--device", default="", help="cuda device, i.e. 0 or 0,1,2,3 or cpu"
-    )
-    parser.add_argument("--view-img", action="store_true", help="display results")
-    parser.add_argument("--save-txt", action="store_true", help="save results to *.txt")
-    parser.add_argument(
-        "--save-conf", action="store_true", help="save confidences in --save-txt labels"
-    )
-    parser.add_argument(
-        "--classes",
-        nargs="+",
-        type=int,
-        help="filter by class: --class 0, or --class 0 2 3",
-    )
-    parser.add_argument(
-        "--agnostic-nms", action="store_true", help="class-agnostic NMS"
-    )
-    parser.add_argument("--augment", action="store_true", help="augmented inference")
-    parser.add_argument("--update", action="store_true", help="update all models")
-    parser.add_argument(
-        "--project", default="runs/detect", help="save results to project/name"
-    )
-    parser.add_argument("--name", default="exp", help="save results to project/name")
-    parser.add_argument(
-        "--exist-ok",
-        action="store_true",
-        help="existing project/name ok, do not increment",
-    )
-    opt = parser.parse_args()
-    print(opt)
-
-    with torch.no_grad():
-        if opt.update:  # update all models (to fix SourceChangeWarning)
-            for opt.weights in ["yolov5s.pt", "yolov5m.pt", "yolov5l.pt", "yolov5x.pt"]:
-                detect()
-                strip_optimizer(opt.weights)
-        else:
-            detect()
+    return save_detected_dir
